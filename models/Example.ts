4ef5336651c3333ea0b54b1dadde70a195b364d9
davydocsurg <davydocsurg@gmail.com>
2022-10-25 14:13:12 +0100

    setup example model

---
+++ models/Example.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema } from "mongoose";
+
+const ExampleSchema: Schema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "The title field is required"],
+            minlength: [
+                5,
+                "Title is shorter than the minimum allowed length (5)",
+            ],
+        },
+
+        description: {
+            type: String,
+            required: [true, "Description is required"],
+            minlength: [
+                10,
+                "Description is shorter than the minimum allowed length (10)",
+            ],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Todo = mongoose.model("Example", ExampleSchema);
+
+export default Todo;
