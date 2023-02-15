2f358fdbaf53b080f479f5dc4c88fefcdcf3ea7c
davydocsurg <davydocsurg@gmail.com>
2023-02-15 16:22:27 +0100

    add custom request types

--- build/config/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mongoDBConnection = void 0;
-const database_1 = require("./database");
-Object.defineProperty(exports, "mongoDBConnection", { enumerable: true, get: function () { return database_1.mongoDBConnection; } });
--- build/helpers/catchAsync.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const catchAsync = (fn) => {
-    return (req, res, next) => {
-        var _a;
-        (_a = fn(req, res, next)) === null || _a === void 0 ? void 0 : _a.catch(next);
-    };
-};
-exports.default = catchAsync;
